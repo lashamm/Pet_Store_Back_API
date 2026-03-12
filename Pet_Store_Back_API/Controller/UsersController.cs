da78837907a1f9958b0eb33673b95d13fed715e1
lasha <maisuradze.llasha@gmail.com>
2026-03-12 23:06:31 +0400

    half done users controller

---
+++ Pet_Store_Back_API/Controller/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pet_Store_Back_API.Controller
+{
+    [Microsoft.AspNetCore.Components.Route("api/controller")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Data.PetStoreContext context;
+
+        public UsersController(Data.PetStoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet("GetUserById/{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = context.Users.FirstOrDefault(p => p.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost("AddUser")]
+        public async Task<IActionResult> AddUser(Models.Entities.User user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+
+    }
+
